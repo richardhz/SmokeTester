306ba9ece334e699e85702963372d776af1803e8
Richard Hernandez <richard.hernandez@ukho.gov.uk>
2023-02-28 18:03:38 +0000

    Tidy

--- SmokeTester/Services/SmokeTestTools.cs
+++ SmokeTester/Services/SmokeTestTools.cs
@@ -1,13 +1,6 @@
-﻿using Microsoft.Win32.SafeHandles;
-using SmokeTester.Data;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using SmokeTester.Data;
 using System.Net.Http.Headers;
-using System.Text;
-using System.Text.Json.Nodes;
-using System.Threading.Tasks;
-using static System.Formats.Asn1.AsnWriter;
+using System.Text.Json;
 
 namespace SmokeTester.Services;
 public sealed class SmokeTestTools : ISmokeTestTools
@@ -17,25 +10,29 @@ public sealed class SmokeTestTools : ISmokeTestTools
 
     public async Task<string> ProcessRequest(SmokeParams smokeParams)
     {
-        string json = "could not aquire access token.";
-
         var accessToken = await GetTokenAsync(smokeParams);
         if (accessToken is null)
         {
             return "could not aquire access token.";
         }
+
         if (smokeParams.Url is null)
         {
             return accessToken;
         }
+
         var client = _httpClientFactory.CreateClient();
         client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
         client.DefaultRequestHeaders.Add("X-Correlation-ID", "00000000-0000-0000-0000-000000000062");
-        //var response = await client.GetAsync(smokeParams.Url);
+
         var response = await client.PostAsync(smokeParams.Url,null);
-        json = await response.Content.ReadAsStringAsync(); 
-       
-        return json;
+        var json = await response.Content.ReadAsStringAsync();
+
+        var jo = JsonSerializer.Deserialize<Object>(json);
+        var options = new JsonSerializerOptions { WriteIndented = true };
+        string jsonString = JsonSerializer.Serialize(jo, options);
+
+        return jsonString;
     }
 
 
