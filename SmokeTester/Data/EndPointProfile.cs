c63e6bc78f89fadf45ad90fe43c809e2abadc6fd
Richard Hernandez <richard.hernandez@ukho.gov.uk>
2023-03-07 19:26:42 +0000

    Ui Changes

--- SmokeTester/Data/EndPointProfile.cs
+++ SmokeTester/Data/EndPointProfile.cs
@@ -10,5 +10,7 @@ public class EndPointProfile
 {
     public string Key { get; set; }
     public string ClientId { get; set; }
+    public string TenantId { get; set; }
     public string BaseUrl { get; set; }
+    public string ActiveUrl { get; set; }
 }
