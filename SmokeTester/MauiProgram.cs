474ca641ea18f014e1e73d2fd91fb153064dce05
Richard Hernandez <r.a.hernandez@outlook.com>
2023-06-03 11:01:55 +0100

    Add azure default creds

--- SmokeTester/MauiProgram.cs
+++ SmokeTester/MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Azure;
+using Microsoft.Extensions.Logging;
 using SmokeTester.Data;
 using SmokeTester.Services;
 
@@ -15,6 +16,16 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 			});
+
+		Environment.SetEnvironmentVariable("AZURE_CLIENT_ID", "40830045-c7e2-48db-8973-37e3f50b0f24");
+		Environment.SetEnvironmentVariable("AZURE_TENANT_ID", "8fda8d81-ebbf-4c25-9b08-2453e217680f");
+		
+		builder.Services.AddSingleton<BlobServiceClient>(c =>
+			new BlobServiceClient(
+				new Uri("https://sbrholding.blob.core.windows.net"),
+				new DefaultAzureCredential()
+				));
+
 
 		builder.Services.AddHttpClient();
 
--- SmokeTester/Services/CloudStorageTools.cs
+++ SmokeTester/Services/CloudStorageTools.cs
@@ -1,51 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Azure.Storage.Blobs;
+﻿using System.Text;
 using Azure.Storage.Blobs.Specialized;
 
 namespace SmokeTester.Services
 {
     public class CloudStorageTools : ICloudStorageTools
     {
-        public IPublicClientApplication IdentityClient { get; set; }
-
-        private async Task<BlockBlobClient> ConnectToAzureStorage(string fileName)
+        private readonly BlobServiceClient _bsc;
+        public CloudStorageTools(BlobServiceClient blobServiceClient)
         {
-            var clientId = "77373c79-5e06-48b8-a974-b91eb2b9cdc0";
-            var tenantId = "8fda8d81-ebbf-4c25-9b08-2453e217680f";
-            string scope = ".default";
-            var fullscope = $"{clientId}/{scope}";
-            IdentityClient = PublicClientApplicationBuilder.Create(clientId)
-                .WithAuthority(AzureCloudInstance.AzurePublic, tenantId)
-                .WithRedirectUri("https://login.microsoftonline.com/common/oauth2/nativeclient")
-                .Build();
+                _bsc = blobServiceClient; 
+        }
 
-            var accounts = await IdentityClient.GetAccountsAsync();
-            var firstAccount = accounts.FirstOrDefault();
-            var authResult = await IdentityClient.AcquireTokenSilent(new[] { fullscope }, firstAccount)
-                                                  .ExecuteAsync();
-
-            //string connectionString = "";
+        private BlockBlobClient ConnectToAzureStorage(string fileName)
+        {
             string containerName = "testtooldata";
             string blobName = $"Team-{fileName}";
 
-            //var bsc = new BlobServiceClient(connectionString);
-
             try
             {
-                var bsc = new BlobServiceClient(new Uri("https://sbrholding.blob.core.windows.net"), new DefaultAzureCredential());
-                var cc = bsc.GetBlobContainerClient(containerName);
+                var cc = _bsc.GetBlobContainerClient(containerName);
                 if (!cc.Exists())
                 {
-                    cc = bsc.CreateBlobContainer(containerName);
+                    cc = _bsc.CreateBlobContainer(containerName);
                 }
                 return cc.GetBlockBlobClient(blobName);
             }
             catch (Exception ex)
             {
+                //display status message
                 var em = ex.Message;
                 throw;
             }
@@ -55,7 +37,7 @@ namespace SmokeTester.Services
         {
             using var ms = new MemoryStream();
             string jsonData = string.Empty;
-            var bbc = await ConnectToAzureStorage(filename);
+            var bbc = ConnectToAzureStorage(filename);
             if (bbc.Exists())
             {
                 await bbc.DownloadToAsync(ms);
@@ -70,7 +52,7 @@ namespace SmokeTester.Services
             byte[] data = Encoding.UTF8.GetBytes(jsonData);
 
             using MemoryStream stream = new MemoryStream(data);
-            var bbc = await ConnectToAzureStorage(filename);
+            var bbc = ConnectToAzureStorage(filename);
             await bbc.UploadAsync(stream);
         }
     }
